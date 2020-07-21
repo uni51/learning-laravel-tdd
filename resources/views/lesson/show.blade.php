27108a116daccb575426b3f0f08c441392affbad
uni51 <uni51@users.noreply.github.com>
2020-07-21 15:52:43 +0900

    3-2 モデルファクトリを使ってデータを整備する

--- resources/views/lesson/show.blade.php
+++ resources/views/lesson/show.blade.php
@@ -1,4 +1,11 @@
 <h1>{{ $lesson->name }}</h1>
 <div>
     <span>空き状況: {{ $lesson->vacancyLevel }}</span>
+    <div>
+        @can('reserve', $lesson)
+            <button class="btn btn-primary">このレッスンを予約する</button>
+        @else
+            <span class="btn btn-primary disabled">予約できません</span>
+        @endcan
+    </div>
 </div>
