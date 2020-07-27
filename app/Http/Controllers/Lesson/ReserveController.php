b92b65160b4d3b019b0fcde0792315c10e197106
uni51 <uni51@users.noreply.github.com>
2020-07-27 15:43:56 +0900

    3-4 MPAのフィーチャーテスト & 3-6 課題

---
+++ app/Http/Controllers/Lesson/ReserveController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Lesson;
+
+use App\Http\Controllers\Controller;
+use App\Models\Lesson;
+use App\Models\Reservation;
+use App\Notifications\ReservationCompleted;
+use Exception;
+use Illuminate\Support\Facades\Auth;
+
+class ReserveController extends Controller
+{
+    public function __invoke(Lesson $lesson)
+    {
+        $user = Auth::user();
+        try {
+            $user->canReserve($lesson);
+        } catch (Exception $e) {
+            return back()->withErrors('予約できません。：' . $e->getMessage());
+        }
+        Reservation::create(['lesson_id' => $lesson->id, 'user_id' => $user->id]);
+
+        $user->notify(new ReservationCompleted($lesson));
+
+        return redirect()->route('lessons.show', ['lesson' => $lesson]);
+    }
+}
